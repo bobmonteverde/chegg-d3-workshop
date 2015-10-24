a9be5737179c19a9257d6d51c1d5ea9a28ad356c
Bob Monteverde <bmonteverde@chegg.com>
2015-10-24 10:23:22 -0700

    Initial templates for Step1

--- chart.js
+++ chart.js
@@ -8,46 +8,15 @@ var cheggMap = function() {
     ;
 
 
-  var projection = d3.geo.albersUsa()
-      .scale(1000);
-
-  var colorScale = d3.scale.linear()
-      //.domain([0, 1.0])
-      .range(['#dddddd', '#1c437b']);
-
-
-  function mapToID(values) {
-    var ret = {};
-
-    values.forEach(function(d) {
-      ret[d.id] = d;
-    });
-
-    return ret;
-  }
-
-
 
   function chart(selection) {
     selection.each(function(data) {
 
       var container = d3.select(this);
-      var dataByID = mapToID(data.data);
-
-      colorScale
-        .domain(d3.extent(data.data, function(d) { return +d.count }));
-
 
       container
           .style('width', width)
           .style('height', height);
-
-
-      projection
-          .translate([width / 2, height / 2]);
-
-      var path = d3.geo.path()
-          .projection(projection);
 
 
       var wrap = container.selectAll('g.chart-wrap').data([data]);
@@ -55,26 +24,7 @@ var cheggMap = function() {
       var gEnter = wrapEnter.append('g');
       var g = wrap.select('g');
 
-      gEnter.append('g').attr('class', 'states')
-
-
       wrap.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-
-      var states = g.select('.states').selectAll('path')
-          .data(topojson.feature(data.map, data.map.objects.states).features)
-
-      states.enter().append('path')
-          .attr('class', 'state')
-          .attr('d', path);
-
-      states.transition().duration(1000)
-          .style('fill', function(d) {
-            var point = dataByID[d.id],
-                val = (point && point.count) || 0;
-
-            return colorScale(val);
-          })
 
 
     });
@@ -97,7 +47,7 @@ var cheggMap = function() {
   };
 
   chart.height = function(_) {
-    if (!arguments.length) return height_;
+    if (!arguments.length) return height;
     height = _;
     return chart;
   };
